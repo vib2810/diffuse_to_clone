f822d0961d4f0f156600630c603963f81bdac9cb
Vibhakar Mohta <vib2810@gmail.com>
2023-11-30 20:08:14 -0500

    initial implementation of test model script

--- src/il_packages/manipulation/src/test_network.py
+++ src/il_packages/manipulation/src/test_network.py
@@ -2,129 +2,75 @@
 #!/usr/bin/env python3
 import sys
 import rospy
-import moveit_commander
-import moveit_msgs.msg
-import geometry_msgs.msg
-import sensor_msgs.msg
 import numpy as np
-from moveit_msgs.msg import PlanningScene, CollisionObject
-from shape_msgs.msg import SolidPrimitive, Mesh
-import scipy.spatial.transform as spt
-import copy
-from geometry_msgs.msg import PoseStamped
-import pickle
-import actionlib
+from geometry_msgs.msg import PoseStamped, Pose
 from il_msgs.msg import RecordJointsAction, RecordJointsResult, RecordJointsGoal
 import torch
 
 sys.path.append("/home/ros_ws/src/il_packages/manipulation/src")
 from moveit_class import moveit_planner
-sys.path.append("/home/ros_ws/")
-from networks.bc_model import BCTrainer
-from networks.residuil_model import ResiduILTrainer
-from networks.lstm_model import LSTMTrainer
-from networks.model_utils import read_pkl_trajectory_data
-import yaml
-import os
-sys.path.append("/home/ros_ws/networks") # for torch.load to work
 
+sys.path.append("/home/ros_ws/")
+from networks.diffusion_model import DiffusionTrainer
 from src.git_packages.frankapy.frankapy import FrankaArm, SensorDataMessageType
 from src.git_packages.frankapy.frankapy import FrankaConstants as FC
 from src.git_packages.frankapy.frankapy.proto_utils import sensor_proto2ros_msg, make_sensor_group_msg
 from src.git_packages.frankapy.frankapy.proto import JointPositionSensorMessage, ShouldTerminateSensorMessage
 from franka_interface_msgs.msg import SensorDataGroup
 from src.il_packages.manipulation.src.moveit_class import get_pose_norm, get_posestamped, EXPERT_RECORD_FREQUENCY
-from geometry_msgs.msg import Pose
+
+sys.path.append("/home/ros_ws/src/git_packages/frankapy")
+from frankapy.proto import PosePositionSensorMessage, CartesianImpedanceSensorMessage
+
+
+sys.path.append("/home/ros_ws/networks") # for torch.load to work
+
 
 class ModelTester:
     # Constants
-    GOAL_THRESHOLD = 0.05
+    GOAL_THRESHOLD = 0.05 # TODO: need to define a goal state or condition to end the model testing
     ACTION_LIMIT_THRESHOLD = 0.2
     SMALL_NORM_CHANGE_BREAK_THRESHOLD = 1e-5
-    NUM_STEPS = 250
+    NUM_STEPS = 500
 
     def __init__(self,
-            model_name,
-            traj_num,
+            model_name
         ):
         # Initialize the model
-        train_params = torch.load("/home/ros_ws/logs/models/" + model_name + ".pt", map_location=torch.device('cpu'))
-        if str(train_params["model_class"]).find("BCTrainer") != -1:
+        stored_pt_file = torch.load("/home/ros_ws/logs/models/" + model_name + ".pt", map_location=torch.device('cpu'))
+        self.train_params = [key for key in stored_pt_file.keys() if key != "model_weights"]
+        if str(stored_pt_file["model_class"]).find("DiffusionTrainer") != -1:
             print("Loading BC Model")
-            self.model = BCTrainer(
-                train_params=train_params,
+            self.model = DiffusionTrainer(
+                train_params=self.train_params,
                 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
             )
-        if str(train_params["model_class"]).find("LSTMTrainer") != -1:
-            print("Loading LSTM Model")
-            self.model = LSTMTrainer(
-                train_params=train_params,
-                device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-            )
-        if str(train_params["model_class"]).find("ResiduILTrainer") != -1:
-            print("Loading ResiduIL Model")
-            self.model = ResiduILTrainer(
-                train_params=train_params,
-                device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-            )
-        self.model.load_model_weights(train_params["model_weights"])
-        self.train_params = train_params
+            self.model.initialize_mpc_action()
+        self.model.load_model_weights(stored_pt_file["model_weights"])
 
         # print model hparams (except model_weights)
         for key in self.train_params:
-            if key != "model_weights":
-                print(key, ":", self.train_params[key])
-
-        # load point and joint ranges from yaml file
-        trajectory_path = '/home/ros_ws/logs/recorded_trajectories/trajectory_' + str(traj_num) + '.yaml'
-        print("Loading limits from: ", trajectory_path)
-        with open(trajectory_path, 'r') as f:
-            self.limits = yaml.load(f, Loader=yaml.FullLoader)
-            self.noise_std = self.limits["noise_std"]
-            self.num_steps_cnfnd = self.limits["num_steps_cnfnd"]
-            self.noise_type = self.limits["noise_type"]
+            print(key, ":", self.train_params[key])
 
         # Put model in eval mode
         self.model.eval()
 
-        # Make folder for saving trajectories
-        self.trajectory_folder = '/home/ros_ws/logs/test_trajectories/'
-        if not(os.path.exists(self.trajectory_folder)):
-            os.makedirs(self.trajectory_folder)
-        self.pickle_file_name = f'{traj_num}__{model_name}.pkl'
-
         # move robot to a valid start position
         self.franka_moveit = moveit_planner()
-
-        # Load goal joints as average of all goal joints in the trajectory
-        _,_,_,_,_,_, self.target_pose = read_pkl_trajectory_data(traj_num, print_data=False)
-        print("Goal Joints loaded form traj_num ", traj_num, "with target pose:\n", self.target_pose)
         
         # Reset joints
         self.franka_moveit.fa.reset_joints()
 
-        if "seq_len" not in self.train_params:
-            self.train_params["seq_len"] = 1
-        # Initialize sequence buffer with zeoros of size seq_len
-        self.seq_buffer = [np.zeros(self.train_params["ac_dim"])]*self.train_params["seq_len"]
-
-        # Initialize prev norm diff for small norm change break
-        self.prev_norm_diff = [None]*5
-    
-
-
-    def move_robot_to_a_start_position(self):
-        """
-        Move robot to start position
-        """
-        start_pose = self.franka_moveit.sample_pose(self.limits, self.target_pose)
-
-        # move robot to start pose
-        self.franka_moveit.goto_pose_moveit(start_pose, speed = 3)
+        if "obs_horizon" not in self.train_params:
+            self.train_params["obs_horizon"] = 1
+        
+        # Initialize sequence buffer with zeoros of size obs_horizon
+        self.seq_buffer = [np.zeros(self.train_params["ac_dim"])]*self.train_params["obs_horizon"]
+        
+        self.prev_joint_norm_diffs = [0]*10 # frames to check for small norm change
 
     def test_model(self, N_EVALS=20):
         eval_counter = 0
-        all_robot_trajectories = {"trajectories": [], "timestamps": [], "tool_poses": [], "target_poses": [], "terminal_reasons": []}
         while not rospy.is_shutdown():
             if(eval_counter>=N_EVALS):
                 break
@@ -135,81 +81,58 @@ class ModelTester:
             np.random.seed(eval_counter*100)
 
             # Move robot to start position
-            self.move_robot_to_a_start_position()
+            self.fa.reset_joints()
             
             # Run the control loop
-            test_robot_joints, test_robot_poses, test_robot_timestamps, terminal_reason = self.test_model_once()
-            all_robot_trajectories["trajectories"].append(test_robot_joints)
-            all_robot_trajectories["tool_poses"].append(test_robot_poses)
-            all_robot_trajectories["timestamps"].append(test_robot_timestamps)
-            all_robot_trajectories["target_poses"].append(self.target_pose)
-            all_robot_trajectories["terminal_reasons"].append(terminal_reason)
-
-            # Save test trajectories to pickle file 
-            with open(self.trajectory_folder + "/" + self.pickle_file_name, 'wb') as f:
-                pickle.dump(all_robot_trajectories, f)
+            self.test_model_once()
                 
         print("Done")
         self.franka_moveit.fa.reset_joints()
     
     def test_model_once(self):
         """
-        Returns test_robot_joints, test_robot_poses, test_robot_timestamps, terminal_reason
+        Run the control loop for one episode
         """
         rate = rospy.Rate(EXPERT_RECORD_FREQUENCY)
 
         # To ensure skill doesn't end before completing trajectory, make the buffer time much longer than needed
-        curr_pose, curr_joints, next_action, terminal_reason = self.get_next_joints()
-        if next_action is None:
-            return [], [], [], None
-        self.franka_moveit.fa.goto_joints(next_action, duration=5, dynamic=True, buffer_time=30, ignore_virtual_walls=True)
+        self.fa.goto_pose(self.fa.get_pose(), duration=120, dynamic=True, buffer_time=10,
+                cartesian_impedances=FC.DEFAULT_TRANSLATIONAL_STIFFNESSES[:3] + FC.DEFAULT_ROTATIONAL_STIFFNESSES)
         
         # Initialize variables to store data
         init_time = rospy.Time.now().to_time()
-        test_robot_joints = []
-        test_robot_poses = []
-        test_robot_timestamps = []
-        
         counter = 0
-        terminal_reason = None
-        u_past = np.zeros((self.num_steps_cnfnd,1))
         while not rospy.is_shutdown():
             # Get curr robot joints, pose and next action
-            curr_pose_rigid, curr_joints, next_action, terminal_reason = self.get_next_joints()
-            if next_action is None:
+            next_pose, next_gripper = self.get_next_action()
+            if next_pose is None:
                 break
-            if self.noise_type == "uniform":
-                u = np.random.uniform(-self.noise_std, self.noise_std, 1)
-            elif self.noise_type == "gaussian":
-                u = np.random.normal(0, self.noise_std, 1)
-            else:
-                u = 0
-            noise = (1 * u + 1 * np.sum(u_past, axis=0))/1
-            next_action += noise
-            u_past = np.roll(u_past, 1, axis=0)
-            u_past[0,:] = u
-
-            # Log the data
-            test_robot_joints.append(curr_joints)
-            test_robot_poses.append(curr_pose_rigid)
-            test_robot_timestamps.append(rospy.Time.now().to_nsec())
             
             # Publish the data
-            traj_gen_proto_msg = JointPositionSensorMessage(
-                id=counter, timestamp=rospy.Time.now().to_time() - init_time, 
-                joints=next_action
+            timestamp = rospy.Time.now().to_time() - init_time
+            traj_gen_proto_msg = PosePositionSensorMessage(
+                id=counter, timestamp=timestamp, 
+                position=[next_pose.position.x, next_pose.position.y, next_pose.position.z],
+                quaternion=[next_pose.orientation.w, next_pose.orientation.x, next_pose.orientation.y, next_pose.orientation.z]
+            )
+            fb_ctrlr_proto = CartesianImpedanceSensorMessage(
+                id=counter, timestamp=timestamp,
+                translational_stiffnesses=FC.DEFAULT_TRANSLATIONAL_STIFFNESSES[:3],
+                rotational_stiffnesses=FC.DEFAULT_ROTATIONAL_STIFFNESSES
             )
             ros_msg = make_sensor_group_msg(
                 trajectory_generator_sensor_msg=sensor_proto2ros_msg(
-                    traj_gen_proto_msg, SensorDataMessageType.JOINT_POSITION)
+                    traj_gen_proto_msg, SensorDataMessageType.POSE_POSITION),
+                feedback_controller_sensor_msg=sensor_proto2ros_msg(
+                    fb_ctrlr_proto, SensorDataMessageType.CARTESIAN_IMPEDANCE)
             )
             self.franka_moveit.pub.publish(ros_msg)
-
+            self.fa.goto_gripper(next_gripper, block=False, speed=0.2)
+            
             # Break if counter exceeds num_steps
             counter += 1
             if counter > self.NUM_STEPS:
                 print(f"Break due to counter {counter} > {self.NUM_STEPS}")
-                terminal_reason = f"Counter Exceeded | {counter} > {self.NUM_STEPS}"
                 break
             rate.sleep()
 
@@ -222,7 +145,7 @@ class ModelTester:
         
         self.franka_moveit.pub.publish(ros_msg)
         self.franka_moveit.fa.stop_skill()
-        return test_robot_joints, test_robot_poses, test_robot_timestamps, terminal_reason
+        return
 
     def terminate_run(self, curr_pose):
         norm_diff = get_pose_norm(curr_pose, self.target_pose)
@@ -244,55 +167,62 @@ class ModelTester:
         self.prev_norm_diff.append(norm_diff)
         return False, None
 
-    def get_next_joints(self):
+    def get_next_action(self):
         """
-        Return curr_pose_rigid, curr_joints, next_action, terminal_reason
+        Return next action of type (Pose, gripper_width)
         """
         # Read current pose and joints
         curr_pose_rigid = self.franka_moveit.fa.get_pose()
         curr_pose = get_posestamped(curr_pose_rigid.translation, [curr_pose_rigid.quaternion[1], curr_pose_rigid.quaternion[2], curr_pose_rigid.quaternion[3], curr_pose_rigid.quaternion[0]]).pose
         curr_joints = self.franka_moveit.fa.get_joints()
+        curr_gripper = self.franka_moveit.fa.get_gripper()
 
         # Append current joints to sequence buffer
         self.seq_buffer.pop(0)
-        self.seq_buffer.append(curr_joints)
-
-        # Check if run should be terminated
-        terminate, terminal_reason = self.terminate_run(curr_pose)
-        if terminate:
-            return curr_pose_rigid, curr_joints, None, terminal_reason
+        self.seq_buffer.append(np.concatenate((np.array(curr_joints), curr_gripper)))
         
         # Get next action
         if len(self.seq_buffer) == 1:
-            joints_tensor = torch.tensor(self.seq_buffer[0]).float().to(self.model.device)
+            # tile the obs to match self.train_params["obs_horizon"] and make of shape (1, self.train_params["obs_horizon"], self.train_params["obs_dim"])
+            obs_tensor = torch.tensor(np.tile(np.expand_dims(np.concatenate(self.seq_buffer), axis=0), (1, self.train_params["obs_horizon"], 1))).float().to(self.model.device)
+            # shape of obs_tensor: (1, self.train_params["obs_horizon"], self.train_params["obs_dim"])
         else:
-            joints_tensor = torch.tensor(np.stack(self.seq_buffer)).float().to(self.model.device)
+            obs_tensor = torch.tensor(np.expand_dims(np.concatenate(self.seq_buffer), axis=0)).float().to(self.model.device)
+            # shape of obs_tensor: (1, self.train_params["obs_horizon"], self.train_params["obs_dim"])
+        
+        # print obs_tensor.shape
+        print(f"obs_tensor: {obs_tensor.shape}")
 
         # forward pass model to get action
-        action = self.model.get_action(joints_tensor, requires_grad=False).squeeze(0).cpu().detach().numpy()
-
+        action = self.model.get_mpc_action(obs_tensor) # np array of shape [self.train_params["ac_dim"]]
+        
+        next_pose = Pose()
+        next_pose.position.x = action[0]
+        next_pose.position.y = action[1]
+        next_pose.position.z = action[2]
+        next_pose.orientation.x = 1; next_pose.orientation.y = 0; next_pose.orientation.z = 0; next_pose.orientation.w = 0
+        next_gripper = action[-1]
+        
         # Check if action is safe
-        norm_diff_joints = np.linalg.norm(action-curr_joints)
+        norm_diff_joints = np.linalg.norm(next_pose.position - curr_pose.position)
         if(norm_diff_joints>self.ACTION_LIMIT_THRESHOLD):
             print(f"Break due to action limits {norm_diff_joints} > {self.ACTION_LIMIT_THRESHOLD}")
             return curr_pose_rigid, curr_joints, None, f"Action Limits | {norm_diff_joints} > {self.ACTION_LIMIT_THRESHOLD}"
         
         # Return next action
-        return curr_pose_rigid, curr_joints, action, None
+        return next_pose, next_gripper
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: please provide model_name as node argument")
-        print("Example: rosrun manipulation test_network.py logs/models/bc_model_3_03-10-2023_05-56-06.pt")
+        print("Example: rosrun manipulation test_network.py logs/models/<model_name>.pt")
         sys.exit()
 
     rospy.init_node('record_trajectories')
-    # sys.argv[1] will be of format logs/models/bc_model_3_03-10-2023_05-56-06.pt
-    traj_num = sys.argv[1].split("_")[2].split(".")[0]
-    model_name = sys.argv[1].split("/")[2].split(".")[0]
+    model_name = sys.argv[1].split('/')[-1].split('.')[0]
 
-    N_EVALS = 20
-    print(f"Testing model {model_name} on trajectory {traj_num} for {N_EVALS} times")
+    N_EVALS = 2
+    print(f"Testing model {model_name} for {N_EVALS} iterations")
     
-    model_tester = ModelTester(model_name, traj_num)
+    model_tester = ModelTester(model_name)
     model_tester.test_model(N_EVALS=N_EVALS)
